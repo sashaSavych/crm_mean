43d664ed62bee816e69da0f2fd3d9c0174b94c25
Oleksandr Savych <oleksandr.i.savych@thomascookonline.com>
2019-01-23 13:16:50 +0200

    add token check for all routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controller = require('../controllers/auth');
 const router = express.Router();
+const passport = require('passport');
 
-router.post('/login', controller.login);
-router.post('/register', controller.register);
+router.post('/login', passport.authenticate('jwt', {session: false}), controller.login);
+router.post('/register', passport.authenticate('jwt', {session: false}), controller.register);
 
 module.exports = router;
