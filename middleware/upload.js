f7affdce74a9e1032827a17f1c8de5ec2b1d7bfb
Oleksandr Savych <oleksandr.i.savych@thomascookonline.com>
2019-01-23 10:44:32 +0200

    add file uploader

---
+++ middleware/upload.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+const moment = require('moment');
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+        const date = moment.format('DDMMYYYY');
+        cb(null, `${date}-${file.originalname}`)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const limits = {
+    fileSize: 1024 * 1024 * 5 // 5MB
+};
+
+module.exports = multer( { storage, fileFilter, limits });
