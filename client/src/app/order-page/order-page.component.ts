7e627ff4868197fa46d25c1cbbb9104bdd19b35b
Oleksandr Savych <oleksandr.i.savych@thomascookonline.com>
2019-02-12 23:59:25 +0200

    add order layouts

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { MaterialHelperService, MaterialModalInstance } from '../shared/services/materialHelperService';
+import { OrderService } from '../shared/services/order.service';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.scss']
+  styleUrls: ['./order-page.component.scss'],
+  providers: [ OrderService ]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, AfterViewInit, OnDestroy {
+  isRootPage = true;
 
-  constructor() { }
+  @ViewChild('orderModal') orderModal: ElementRef;
+  modalWindow: MaterialModalInstance;
+
+  constructor(private router: Router,
+              private orderService: OrderService) { }
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => this.isRootPage = this.router.url === '/order');
   }
 
+  showModal() {
+    this.modalWindow.open();
+  }
+
+  onModalSubmit() {
+    this.modalWindow.close();
+  }
+
+  ngAfterViewInit() {
+    this.modalWindow = MaterialHelperService.initModal(this.orderModal);
+  }
+
+  ngOnDestroy() {
+    this.modalWindow.destroy();
+  }
 }
---
+++ client/src/app/shared/services/order.service.ts
@@ -0,0 +1,11 @@
+export class OrderService {
+  addOrder() {
+
+  }
+
+  removeOrder() {
+
+  }
+
+  clear() {}
+}
